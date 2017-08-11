d33423770f8c6f6bf7194f195000a59d43b1e5a1
ankit yadav <drkkgy@gmail.com>
2017-08-11 13:33:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res){
-   res.sendFile(path.join(--dirname,'ui','article-one.html'));
+   res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
 app.get('/article-one/two',function (req,res){
    res.send('hey its article 1.2 being served by the server'); 
