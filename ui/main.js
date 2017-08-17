dad7e3777318ee9b346da3ab559efe9c871a2cfc
ankit yadav <drkkgy@gmail.com>
2017-08-18 01:04:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,8 +36,8 @@ submit.onclick = function()
     //
     var names = ['name1','name2','name3' , 'name4'];
     var list = '';
-    for (var i =0 ; i< name.length; i++){
-    list +=' <li>' + name[i] + '</li>';
+    for (var i =0 ; i< names.length; i++){
+    list +=' <li>' + names[i] + '</li>';
         }
         var ul = document.getElementById('namelist');
         ul.innerHTML = list;
