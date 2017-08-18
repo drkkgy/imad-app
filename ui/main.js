3058c44190511590000ef4497e292ed0d236461f
ankit yadav <drkkgy@gmail.com>
2017-08-18 14:59:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick = function()
 {
     //make a request to thebserver 
     //cpture the list of name and render it as a list
-    var names[] = ['name1','name2','name3' , 'name4'];
+    var names = ['name1','name2','name3' , 'name4'];
     var list = '';
     for (var i=0;i < names.length;i++){
     list +='<li>' + names[i] + '</li>';
