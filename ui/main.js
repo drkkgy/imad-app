763fd16b9edb169aa4326c16239fe16cd4485c4d
ankit yadav <drkkgy@gmail.com>
2017-08-18 10:18:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,8 +32,8 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    //
-    //
+    //make a request to thebserver 
+    //cpture the list of name and render it as a list
     var names = ['name1','name2','name3' , 'name4'];
     var list = '';
     for (var i =0 ; i< names.length; i++){
